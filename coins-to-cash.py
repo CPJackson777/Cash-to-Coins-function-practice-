0d3cb69eda2f7d5c96f4caba0f70987bf0a30d45
Charles Jackson <cpjackson572@gmail.com>
2020-01-15 09:45:40 -0600

    added additional methos to solve that Sophia showed the class

--- coins-to-cash.py
+++ coins-to-cash.py
@@ -32,3 +32,31 @@ def make_change():
     return piggy_bank
 
 print(make_change())
+
+# **************ALTERNATE WAY TO SOLVE THIS*****************
+
+# def fill_piggy_bank (dollarAmount):
+#     quarters_quantity = 0
+#     dimes_quantity = 0
+#     nickels_quantity = 0
+#     pennies_quantity = 0
+#     while dollarAmount > .25:
+#         dollarAmount -= .25
+#         quarters_quantity += 1
+#     while dollarAmount > .1:
+#         dollarAmount -= .1
+#         dimes_quantity += 1
+#     while dollarAmount > .05:
+#         dollarAmount -= .05
+#         nickels_quantity += 1
+#     while dollarAmount > .01:
+#         dollarAmount -= .01
+#         pennies_quantity += 1
+#     piggyBank.update({
+#         'quarters': quarters_quantity,
+#         'dimes': dimes_quantity,
+#         'nickels': nickels_quantity,
+#         'pennies': pennies_quantity
+#     })
+#     print(piggyBank)
+# fill_piggy_bank(dollarAmount)
